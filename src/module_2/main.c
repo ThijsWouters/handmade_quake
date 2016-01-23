612900b77926043478e5ac26d4cb3779e5f527e7
Thijs Wouters <thijs@morewood.be>
2016-01-23 01:55:00 +0100

    Exit window on a keypress

--- src/module_2/main.c
+++ src/module_2/main.c
@@ -1,36 +1,45 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <X11/Xlib.h>
+#include <stdio.h>
+
+int8_t IsRunning = 1;
 
 int32_t main(int32_t argc, char* argv[]) {
   Display *display;
-  Visual *visual;
-  int depth;
-  XSetWindowAttributes frame_attributes;
   Window frame_window;
-  XKeyEvent event;
+  XEvent event;
 
   display = XOpenDisplay(NULL);
-  visual = DefaultVisual(display, 0);
-  depth = DefaultDepth(display, 0);
 
-  frame_attributes.background_pixel = XWhitePixel(display, 0);
-
-  frame_window = XCreateWindow(display, XRootWindow(display, 0),
-      0, 0, 400, 400, 5, depth,
-      InputOutput, visual, CWBackPixel,
-      &frame_attributes);
+  frame_window = XCreateSimpleWindow(display, XRootWindow(display, 0),
+      0, 0, 800, 600, 0, XBlackPixel(display, 0), XBlackPixel(display, 0));
 
   XStoreName(display, frame_window, "Handmade Quake");
-  XSelectInput(display, frame_window, ExposureMask | StructureNotifyMask);
+  XSelectInput(display, frame_window,
+      KeyPressMask | KeyReleaseMask);
+
+  Atom WM_DELETE_WINDOW = XInternAtom(display, "WM_DELETE_WINDOW", False);
+  XSetWMProtocols(display, frame_window, &WM_DELETE_WINDOW, 1);
 
   XMapWindow(display, frame_window);
 
-  while(1) {
-    XNextEvent(display, (XEvent*) &event);
-    switch(event.type) {
-      default:
-        break;
+  while(IsRunning) {
+    if (XEventsQueued(display, QueuedAfterFlush) > 0) {
+      XNextEvent(display, (XEvent*) &event);
+
+      switch(event.type) {
+        case KeyPress:
+          IsRunning = 0;
+          break;
+        case ClientMessage:
+          if (event.xclient.data.l[0] == WM_DELETE_WINDOW) {
+            IsRunning = 0;
+          }
+          break;
+        default:
+          break;
+      }
     }
   }
 
