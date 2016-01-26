5a0ee3fe77174f80ebe1efdfb80bd36c2aa8f861
Thijs Wouters <thijs@morewood.be>
2016-01-26 21:46:43 +0100

    Calculate time between game loops

--- src/module_2/main.c
+++ src/module_2/main.c
@@ -2,8 +2,19 @@
 #include <stdlib.h>
 #include <X11/Xlib.h>
 #include <stdio.h>
+#include <time.h>
 
 int8_t IsRunning = 1;
+
+double time_in_sec(){
+  struct timespec time;
+  double result;
+  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time);
+  result = (double) time.tv_nsec;
+  result /= 1000000000.0;
+  result += (double) time.tv_sec;
+  return result;
+}
 
 int32_t main(int32_t argc, char* argv[]) {
   Display *display;
@@ -24,6 +35,11 @@ int32_t main(int32_t argc, char* argv[]) {
 
   XMapWindow(display, frame_window);
 
+  double seconds_per_clock = 1.0 / (double) CLOCKS_PER_SEC;
+
+  double tick, tock;
+  tick = time_in_sec();
+
   while(IsRunning) {
     if (XEventsQueued(display, QueuedAfterFlush) > 0) {
       XNextEvent(display, (XEvent*) &event);
@@ -41,7 +57,14 @@ int32_t main(int32_t argc, char* argv[]) {
           break;
       }
     }
+
+    tock = time_in_sec();
+
+    double seconds_gone_by = tock - tick;
+    tick = tock;
+    printf("%3.9f\n", seconds_gone_by);
   }
 
   return 0;
 }
+
